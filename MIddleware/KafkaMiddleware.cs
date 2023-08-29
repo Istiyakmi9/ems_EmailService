56ae2cc6e02de902eeacbfe629cde7daca3c34d2
md.istiyak <md.istiyak@primesoftinc.com>
2023-08-29 13:05:46 +0530

    deploy new service

--- MIddleware/KafkaMiddleware.cs
+++ MIddleware/KafkaMiddleware.cs
@@ -1,36 +1,38 @@
 ﻿using Confluent.Kafka;
+using Microsoft.Extensions.Options;
+using ModalLayer;
 
 namespace EmailRequest.MIddleware
 {
     public class KafkaMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly KafkaServiceConfig _kafkaServiceConfig;
 
-        public KafkaMiddleware(RequestDelegate next)
+        public KafkaMiddleware(RequestDelegate next, IOptions<KafkaServiceConfig> options)
         {
             _next = next;
+            _kafkaServiceConfig = options.Value;
         }
 
         public async Task Invoke(HttpContext context)
         {
             try
-            {
-                
+            {                
                 var config = new ConsumerConfig
                 {
-                    GroupId = "gid-consumers",
-                    BootstrapServers = "localhost:9092"
+                    BootstrapServers = $"{_kafkaServiceConfig.ServiceName}:{_kafkaServiceConfig.Port}"
                 };
+
                 using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
                 {
-                    consumer.Subscribe("testdata");
+                    consumer.Subscribe(_kafkaServiceConfig.AttendanceEmailTopic);
                     while (true)
                     {
                         var message = consumer.Consume();
                         Console.WriteLine(message.Message.Value);
                     }
                 }
-
             }
             catch (Exception)
             {
