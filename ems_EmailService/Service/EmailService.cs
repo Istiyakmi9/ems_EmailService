f4f446fcd88647593de9ceddbbd1cdab1880b679
md.istiyak <md.istiyak@primesoftinc.com>
2023-08-31 16:56:39 +0530

    added try catch

--- ems_EmailService/Service/EmailService.cs
+++ ems_EmailService/Service/EmailService.cs
@@ -36,63 +36,63 @@ namespace EmalRequest.Service
 
         public async Task SendEmail(EmailSenderModal emailSenderModal)
         {
-            if (emailSenderModal == null || emailSenderModal.To == null || emailSenderModal.To.Count == 0)
-                throw new HiringBellException("To send email receiver address is mandatory. Receiver address not found.");
-
-            EmailSettingDetail _emailSettingDetail = GetEmailSettingDetail();
-
-            var fromAddress = new System.Net.Mail.MailAddress(_emailSettingDetail!.EmailAddress, emailSenderModal.Title);
-
-            _logger.LogInformation($"[7. Kafka] Reading email setting read successfully.");
-
-            var smtp = new SmtpClient
-            {
-                Host = _emailSettingDetail.EmailHost,
-                Port = _emailSettingDetail.PortNo,
-                EnableSsl = _emailSettingDetail.EnableSsl,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = _emailSettingDetail.UserDefaultCredentials,
-                Credentials = new NetworkCredential(fromAddress.Address, _emailSettingDetail.Credentials)
-            };
-
-            _logger.LogInformation($"[8. Kafka] Configuring image path and body content.");
-            var message = new MailMessage();
-            var logoPath = Path.Combine(_fileLocationDetail.LogoPath, ApplicationConstants.HiringBellLogoSmall);
-
-            // Create the HTML view  
-            AlternateView htmlView = AlternateView.CreateAlternateViewFromString(emailSenderModal.Body, Encoding.UTF8, MediaTypeNames.Text.Html);
-
-            // Create a plain text message for client that don't support HTML  
-            string mediaType = MediaTypeNames.Image.Jpeg;
-            LinkedResource img = new LinkedResource(logoPath, mediaType);
-
-            // Make sure you set all these values!!!  
-            img.ContentId = "logo";
-            img.ContentType.MediaType = mediaType;
-            img.TransferEncoding = TransferEncoding.Base64;
-            img.ContentType.Name = img.ContentId;
-            img.ContentLink = new Uri("cid:" + img.ContentId);
-            htmlView.LinkedResources.Add(img);
-            message.AlternateViews.Add(htmlView);
-
-            message.Subject = emailSenderModal.Subject;
-            //message.Body = emailSenderModal.Body;
-            message.IsBodyHtml = true;
-            message.From = fromAddress;
-
-            foreach (var emailAddress in emailSenderModal.To)
-                message.To.Add(emailAddress);
-
-            if (emailSenderModal.CC != null && emailSenderModal.CC.Count > 0)
-                foreach (var emailAddress in emailSenderModal.CC)
-                    message.CC.Add(emailAddress);
-
-            if (emailSenderModal.BCC != null && emailSenderModal.BCC.Count > 0)
-                foreach (var emailAddress in emailSenderModal.BCC)
-                    message.Bcc.Add(emailAddress);
-
             try
             {
+                if (emailSenderModal == null || emailSenderModal.To == null || emailSenderModal.To.Count == 0)
+                    throw new HiringBellException("To send email receiver address is mandatory. Receiver address not found.");
+
+                EmailSettingDetail _emailSettingDetail = GetEmailSettingDetail();
+
+                var fromAddress = new System.Net.Mail.MailAddress(_emailSettingDetail!.EmailAddress, emailSenderModal.Title);
+
+                _logger.LogInformation($"[7. Kafka] Reading email setting read successfully.");
+
+                var smtp = new SmtpClient
+                {
+                    Host = _emailSettingDetail.EmailHost,
+                    Port = _emailSettingDetail.PortNo,
+                    EnableSsl = _emailSettingDetail.EnableSsl,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = _emailSettingDetail.UserDefaultCredentials,
+                    Credentials = new NetworkCredential(fromAddress.Address, _emailSettingDetail.Credentials)
+                };
+
+                _logger.LogInformation($"[8. Kafka] Configuring image path and body content.");
+                var message = new MailMessage();
+                var logoPath = Path.Combine(_fileLocationDetail.LogoPath, ApplicationConstants.HiringBellLogoSmall);
+
+                // Create the HTML view  
+                AlternateView htmlView = AlternateView.CreateAlternateViewFromString(emailSenderModal.Body, Encoding.UTF8, MediaTypeNames.Text.Html);
+
+                // Create a plain text message for client that don't support HTML  
+                string mediaType = MediaTypeNames.Image.Jpeg;
+                LinkedResource img = new LinkedResource(logoPath, mediaType);
+
+                // Make sure you set all these values!!!  
+                img.ContentId = "logo";
+                img.ContentType.MediaType = mediaType;
+                img.TransferEncoding = TransferEncoding.Base64;
+                img.ContentType.Name = img.ContentId;
+                img.ContentLink = new Uri("cid:" + img.ContentId);
+                htmlView.LinkedResources.Add(img);
+                message.AlternateViews.Add(htmlView);
+
+                message.Subject = emailSenderModal.Subject;
+                //message.Body = emailSenderModal.Body;
+                message.IsBodyHtml = true;
+                message.From = fromAddress;
+
+                foreach (var emailAddress in emailSenderModal.To)
+                    message.To.Add(emailAddress);
+
+                if (emailSenderModal.CC != null && emailSenderModal.CC.Count > 0)
+                    foreach (var emailAddress in emailSenderModal.CC)
+                        message.CC.Add(emailAddress);
+
+                if (emailSenderModal.BCC != null && emailSenderModal.BCC.Count > 0)
+                    foreach (var emailAddress in emailSenderModal.BCC)
+                        message.Bcc.Add(emailAddress);
+
                 if (emailSenderModal.FileDetails != null && emailSenderModal.FileDetails.Count > 0)
                 {
                     foreach (var files in emailSenderModal.FileDetails)
@@ -110,7 +110,7 @@ namespace EmalRequest.Service
             }
             catch (Exception ex)
             {
-                _logger.LogError($"[9. Kafka] Sending email got exception. Messge: {ex.Message}");
+                _logger.LogError($"[Kafka] Sending email got exception. Messge: {ex.Message}");
                 throw;
             }
 
