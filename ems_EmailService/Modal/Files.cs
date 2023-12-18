5fbb5ed6cb5ab0d7675eed216fbf9ea28f1b9d0f
Marghub <marghub12@gmail.com>
2023-12-18 20:20:09 +0530

    Primary logo get method added

---
+++ ems_EmailService/Modal/Files.cs
@@ -0,0 +1,14 @@
+﻿namespace EmailRequest.Modal
+{
+    public class Files
+    {
+        public int CompanyFileId { get; set; }
+        public int FileId { get; set; }
+        public int CompanyId { set; get; }
+        public string FileName { set; get; }
+        public string FileDescription { set; get; }
+        public string FileExtension { set; get; }
+        public string FilePath { set; get; }
+        public string FileRole { set; get; }
+    }
+}
